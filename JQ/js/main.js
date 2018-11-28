ce8f04e3b8cbb83976a587e3fe56314e591ad565
imaginationsabyss <42756540+imaginationsabyss@users.noreply.github.com>
2018-11-27 22:57:50 -0500

    up up up and away update

--- JQ/js/main.js
+++ JQ/js/main.js
@@ -1,17 +1,19 @@
 // JavaScript Document
-$(".box").click(function(){
-$("h6").html("It is about all of the superhero's eatting the non mutants");});
+$(".five").click(function(){
+	$(".five h2").text("It is about all of the superhero's eatting the non mutants");
+});
 
-$(".box").mouseup(function(){
-$("h4").html("This is a comic about human like fish detectives");});
+$(".four").mouseup(function(){
+	$(".four h3").html("This is a comic about human like fish detectives");
+});
 
-$(".box").mouseover(function(){
-$("h3").html("This story is about what happens if you are stuck in a Wizard of OZ type situation");});
+$(".three").mouseover(function(){
+$(".three h4").html("This story is about what happens if you are stuck in a Wizard of OZ type situation");});
 
-$(".box").mouseenter(function(){
-$("h2").html("Funny comic about an over powered superhero with the mind of a child");});
+$(".two").mouseenter(function(){
+$(".two h5").html("Funny comic about an over powered superhero with the mind of a child");});
 
-$(".box").mousemove(function(){
-$("h5").html("What if? is about changing a pivotal part of a story and seeing the different outcomes, what if aunt May was doc oct and spiderman was a midget?");});
+$(".one").mousemove(function(){
+$(".one h6").html("What if? is about changing a pivotal part of a story and seeing the different outcomes, what if aunt May was doc oct and spiderman was a midget?");});
 
 
