eb6e5cb0ef3ad09ed446dd3f04fba249d13da4d3
imaginationsabyss <42756540+imaginationsabyss@users.noreply.github.com>
2018-11-29 00:29:41 -0500

    updated

---
+++ Guessing game/js/main.js
@@ -0,0 +1,27 @@
+var x = Math.floor(Math.random() * 100 + 1);
+var turns = 5;
+ 
+$('input').change(function() {
+  var guess = $('input').val();
+  if (!guess) return;
+  guess = Number(guess);
+  var hint;
+  if (guess == x) {
+    hint = '<p>YOU WIN!</p>' +
+      '<img src="/images/photo/gold_trophy.jpg">';
+    turns = 0;
+  } else {
+    hint = 'Nope.';
+    if (guess < x) hint += ' Too small!';
+    if (guess > x) hint += ' Too big!';
+    turns = turns - 1;
+  }
+  if (turns == 0) {
+    hint += '<div>The secret number was ' + x + '.</div>';
+    $('p').hide();
+  } else {
+    hint += ' You have ' + turns + ' guesses left.';
+  }
+  $('div').html(hint);
+  $(this).select();
+});
