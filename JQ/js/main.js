a29ca3b0452aac991c05c40975902c8b3e31d63d
imaginationsabyss <42756540+imaginationsabyss@users.noreply.github.com>
2018-12-13 11:37:23 -0500

    updates

---
+++ JQ/js/main.js
@@ -0,0 +1,19 @@
+// JavaScript Document
+$(".five").click(function(){
+	$(".five h3").text("It is about all of the superhero's eatting the non mutants");
+});
+
+$("four").mouseup(function(){
+	$(".four h3").html("This is a comic about human like fish detectives");
+});
+
+$("h3").mouseover(function(){
+$("h3").html("This story is about what happens if you are stuck in a Wizard of OZ type situation");});
+
+$(".box").mouseenter(function(){
+$("h2").html("Funny comic about an over powered superhero with the mind of a child");});
+
+$(".box").mousemove(function(){
+$("h5").html("What if? is about changing a pivotal part of a story and seeing the different outcomes, what if aunt May was doc oct and spiderman was a midget?");});
+
+
